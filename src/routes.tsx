92b28e427d0c80eb017b93b00fd4918cdc284cb7
LuNe137 <luizferreirafariasn@gmail.com>
2022-08-17 19:18:03 -0300

    new feature menu page and some updates in notfound page/menu

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'components/Menu';
 import Sobre from 'pages/Sobre';
+import Prato from 'pages/Prato';
 import NotFound from 'pages/NotFound';
 import Footer from 'components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -17,8 +18,9 @@ export default function AppRouter(){
             <Route index element={<Inicio />} />
             <Route path='/cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
+            <Route path='prato/:id' element={<Prato />} />
           </ Route>
-          <Route path='*' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} /> 
         </Routes>
         <Footer />
       </Router>  
